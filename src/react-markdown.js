48f32b0b26fe61e28495bb87fc1fe88727cc8e4c
Espen Hovlandsdal <rexxars@gmail.com>
2015-05-19 02:52:40 +0200

    Initial working version

--- src/react-markdown.js
+++ src/react-markdown.js
@@ -1,16 +1,37 @@
 'use strict';
 
 var React = require('react');
+var CommonMark = require('commonmark');
+var ReactRenderer = require('commonmark-react-renderer');
+
+var parser = new CommonMark.Parser();
 
 var ReactMarkdown = React.createClass({
     displayName: 'ReactMarkdown',
 
     propTypes: {
-        text: React.PropTypes.string.isRequired
+        source: React.PropTypes.string.isRequired,
+        containerTagName: React.PropTypes.string,
+        sourcePos: React.PropTypes.bool,
+        escapeHtml: React.PropTypes.bool,
+        skipHtml: React.PropTypes.bool,
+        softBreak: React.PropTypes.string
+    },
+
+    getDefaultProps: function() {
+        return {
+            containerTagName: 'div'
+        };
     },
 
     render: function() {
-        return <div>{this.props.text}</div>;
+        var renderer = new ReactRenderer(this.props);
+        var ast = parser.parse(this.props.source || '');
+
+        return React.createElement.apply(React,
+            [this.props.containerTagName, null]
+                .concat(renderer.render(ast))
+        );
     }
 });
 
--- test/react-markdown.test.js
+++ test/react-markdown.test.js
@@ -10,19 +10,85 @@ var TestUtils = React.addons.TestUtils;
 describe('ReactMarkdown', function() {
     jsdom();
 
-    var testText = 'I was rendered using React at ' + (new Date()).toString();
+    var testDate = (new Date()).toString();
+    var testMarkdown = [
+        '# Demo\n\n',
+        'I was *rendered* using __React__ at ' + testDate,
+        ' and it was so much FUN!', '\n\n',
+        'Lets do it <i>again</i>!\nYeah?'
+    ].join('');
 
-    before('render and locate element', function() {
+    it('should have rendered a div with the right children', function() {
         this.rendered = TestUtils.renderIntoDocument(
-            <ReactMarkdown text={testText} />
+            React.createElement(ReactMarkdown, { source: testMarkdown })
         );
+
+        expect(this.rendered.getDOMNode().tagName).to.equal('DIV');
+        expect(this.rendered.getDOMNode().innerHTML).to.contain(testDate);
+
+        var h1 = TestUtils.findRenderedDOMComponentWithTag(this.rendered, 'h1');
+        expect(h1.getDOMNode().innerHTML).to.equal('Demo');
+
+        var em = TestUtils.findRenderedDOMComponentWithTag(this.rendered, 'em');
+        expect(em.getDOMNode().innerHTML).to.equal('rendered');
+
+        var strong = TestUtils.findRenderedDOMComponentWithTag(this.rendered, 'strong');
+        expect(strong.getDOMNode().innerHTML).to.equal('React');
+
+        var ps = TestUtils.scryRenderedDOMComponentsWithTag(this.rendered, 'p');
+        expect(ps).to.have.length(2);
     });
 
-    it('should have passed text prop as inner text', function() {
-        expect(this.rendered.getDOMNode().innerHTML).to.equal(testText);
+    it('can specify different container tag name', function() {
+        this.rendered = TestUtils.renderIntoDocument(
+            React.createElement(ReactMarkdown, { source: testMarkdown, containerTagName: 'section' })
+        );
+
+        expect(this.rendered.getDOMNode().tagName).to.equal('SECTION');
     });
 
-    it('should not pass before tests are updated', function() {
-        expect(false, 'tests implemented').to.equal(true);
+    it('can be told to use <br> for soft-breaks', function() {
+        this.rendered = TestUtils.renderIntoDocument(
+            React.createElement(ReactMarkdown, { source: testMarkdown, softBreak: 'br' })
+        );
+
+        var strong = TestUtils.findRenderedDOMComponentWithTag(this.rendered, 'br');
+        expect(strong).to.be.ok;
+    });
+
+    it('can be told to output sourcemaps as data attributes', function() {
+        this.rendered = TestUtils.renderIntoDocument(
+            React.createElement(ReactMarkdown, { source: testMarkdown, sourcePos: true })
+        );
+
+        var h1 = TestUtils.findRenderedDOMComponentWithTag(this.rendered, 'h1');
+        expect(h1.getDOMNode().getAttribute('data-sourcepos')).to.equal('1:1-1:6');
+    });
+
+    it('can be told to escape html', function() {
+        this.rendered = TestUtils.renderIntoDocument(
+            React.createElement(ReactMarkdown, { source: testMarkdown, escapeHtml: true })
+        );
+
+        var ps = TestUtils.scryRenderedDOMComponentsWithTag(this.rendered, 'p');
+        expect(ps[1].getDOMNode().innerHTML).to.contain('&lt;i&gt;');
+    });
+
+    it('can be told to skip HTML', function() {
+        this.rendered = TestUtils.renderIntoDocument(
+            React.createElement(ReactMarkdown, { source: testMarkdown, skipHtml: true })
+        );
+
+        var ps = TestUtils.scryRenderedDOMComponentsWithTag(this.rendered, 'p');
+        expect(ps[1].getDOMNode().innerHTML).to.not.contain('<i>');
+        expect(ps[1].getDOMNode().innerHTML).to.not.contain('&lt;i&gt;');
+    });
+
+    it('does not blow up if no source is given', function() {
+        this.rendered = TestUtils.renderIntoDocument(
+            React.createElement(ReactMarkdown, {})
+        );
+
+        expect(this.rendered.getDOMNode().innerHTML).to.equal('');
     });
 });
