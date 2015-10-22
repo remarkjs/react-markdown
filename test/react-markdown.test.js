f53a288d50219c6a0ede9f0cf78799fc639d541f
Espen Hovlandsdal <rexxars@gmail.com>
2015-10-22 10:04:24 +0200

    Update dependencies. Require React >= 0.14.

--- test/react-markdown.test.js
+++ test/react-markdown.test.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var React = require('react/addons'),
+var React = require('react'),
+    ReactDom = require('react-dom'),
+    TestUtils = require('react-addons-test-utils'),
     jsdom = require('mocha-jsdom'),
     expect = require('chai').expect,
     ReactMarkdown = require('../');
-
-var TestUtils = React.addons.TestUtils;
 
 describe('ReactMarkdown', function() {
     jsdom();
@@ -23,7 +23,7 @@ describe('ReactMarkdown', function() {
             React.createElement(ReactMarkdown, { source: testMarkdown })
         );
 
-        expect(rendered.getDOMNode().getAttribute('class')).to.equal(null);
+        expect(ReactDom.findDOMNode(rendered).getAttribute('class')).to.equal(null);
     });
 
     it('should set a class on container if className is passed as prop', function() {
@@ -31,7 +31,7 @@ describe('ReactMarkdown', function() {
             React.createElement(ReactMarkdown, { source: testMarkdown, className: 'foo bar' })
         );
 
-        expect(rendered.getDOMNode().getAttribute('class')).to.equal('foo bar');
+        expect(ReactDom.findDOMNode(rendered).getAttribute('class')).to.equal('foo bar');
     });
 
     it('should have rendered a div with the right children', function() {
@@ -39,17 +39,17 @@ describe('ReactMarkdown', function() {
             React.createElement(ReactMarkdown, { source: testMarkdown })
         );
 
-        expect(rendered.getDOMNode().tagName).to.equal('DIV');
-        expect(rendered.getDOMNode().innerHTML).to.contain(testDate);
+        expect(ReactDom.findDOMNode(rendered).tagName).to.equal('DIV');
+        expect(ReactDom.findDOMNode(rendered).innerHTML).to.contain(testDate);
 
         var h1 = TestUtils.findRenderedDOMComponentWithTag(rendered, 'h1');
-        expect(h1.getDOMNode().innerHTML).to.equal('Demo');
+        expect(ReactDom.findDOMNode(h1).innerHTML).to.equal('Demo');
 
         var em = TestUtils.findRenderedDOMComponentWithTag(rendered, 'em');
-        expect(em.getDOMNode().innerHTML).to.equal('rendered');
+        expect(ReactDom.findDOMNode(em).innerHTML).to.equal('rendered');
 
         var strong = TestUtils.findRenderedDOMComponentWithTag(rendered, 'strong');
-        expect(strong.getDOMNode().innerHTML).to.equal('React');
+        expect(ReactDom.findDOMNode(strong).innerHTML).to.equal('React');
 
         var ps = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'p');
         expect(ps).to.have.length(2);
@@ -60,7 +60,7 @@ describe('ReactMarkdown', function() {
             React.createElement(ReactMarkdown, { source: testMarkdown, containerTagName: 'section' })
         );
 
-        expect(rendered.getDOMNode().tagName).to.equal('SECTION');
+        expect(ReactDom.findDOMNode(rendered).tagName).to.equal('SECTION');
     });
 
     it('can be told to use <br> for soft-breaks', function() {
@@ -78,7 +78,7 @@ describe('ReactMarkdown', function() {
         );
 
         var h1 = TestUtils.findRenderedDOMComponentWithTag(rendered, 'h1');
-        expect(h1.getDOMNode().getAttribute('data-sourcepos')).to.equal('1:1-1:6');
+        expect(ReactDom.findDOMNode(h1).getAttribute('data-sourcepos')).to.equal('1:1-1:6');
     });
 
     it('can be told to escape html', function() {
@@ -87,7 +87,7 @@ describe('ReactMarkdown', function() {
         );
 
         var ps = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'p');
-        expect(ps[1].getDOMNode().innerHTML).to.contain('&lt;i&gt;');
+        expect(ReactDom.findDOMNode(ps[1]).innerHTML).to.contain('&lt;i&gt;');
     });
 
     it('can be told to skip HTML', function() {
@@ -96,8 +96,8 @@ describe('ReactMarkdown', function() {
         );
 
         var ps = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'p');
-        expect(ps[1].getDOMNode().innerHTML).to.not.contain('<i>');
-        expect(ps[1].getDOMNode().innerHTML).to.not.contain('&lt;i&gt;');
+        expect(ReactDom.findDOMNode(ps[1]).innerHTML).to.not.contain('<i>');
+        expect(ReactDom.findDOMNode(ps[1]).innerHTML).to.not.contain('&lt;i&gt;');
     });
 
     it('does not blow up if no source is given', function() {
@@ -105,6 +105,6 @@ describe('ReactMarkdown', function() {
             React.createElement(ReactMarkdown, {})
         );
 
-        expect(rendered.getDOMNode().innerHTML).to.equal('');
+        expect(ReactDom.findDOMNode(rendered).innerHTML).to.equal('');
     });
 });
