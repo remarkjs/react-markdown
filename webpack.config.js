27710762b99f5356a1992b666585ea91647e3da3
Espen Hovlandsdal <espen@hovlandsdal.com>
2015-11-28 22:38:25 +0100

    Fix issue with react external name

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,18 @@ var config = {
     },
 
     externals: {
-        'react': 'React',
-        'react-dom': 'ReactDOM'
+        'react': {
+            root: 'React',
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom'
+        }
     },
 
     module: {
