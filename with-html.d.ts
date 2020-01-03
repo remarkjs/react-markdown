eebdeb7f92389fa643fdf07c883536da2a23c20a
Peng Guanwen <pg999w@outlook.com>
2020-01-03 18:34:48 +0800

    Fix typescript declaration

---
+++ with-html.d.ts
@@ -0,0 +1,5 @@
+import {Component} from 'react'
+import ReactMarkdownRoot from 'react-markdown'
+
+export default class ReactMarkdown extends Component<ReactMarkdownRoot.ReactMarkdownProps, {}> {}
+
