c17b71ddad02a54a7121f67d1f2149989640b36c
Remco Haszing <remcohaszing@gmail.com>
2024-06-28 14:23:21 +0200

    Leverage TypeScript project references

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @typedef {import('hast-util-to-jsx-runtime').ExtraProps} ExtraProps
- * @typedef {import('./lib/index.js').AllowElement} AllowElement
- * @typedef {import('./lib/index.js').Components} Components
- * @typedef {import('./lib/index.js').Options} Options
- * @typedef {import('./lib/index.js').UrlTransform} UrlTransform
- */
-
-export {Markdown as default, defaultUrlTransform} from './lib/index.js'
