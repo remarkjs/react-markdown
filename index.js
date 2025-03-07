939c6671c9dbffccfe8e27bba256f62405031193
Remco Haszing <remcohaszing@gmail.com>
2025-03-07 11:01:56 +0100

    Add `fallback` prop to `MarkdownHooks`
    The `<MarkdownHooks/>` component now supports a new prop named
    `fallback`. This fallback is displayed while the initial content hasn’t
    loaded yet. The name `fallback` was chosen to match the same prop from
    `<Suspense/>`.
    
    Closes GH-897.
    
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @typedef {import('./lib/index.js').AllowElement} AllowElement
  * @typedef {import('./lib/index.js').Components} Components
  * @typedef {import('./lib/index.js').ExtraProps} ExtraProps
+ * @typedef {import('./lib/index.js').HooksOptions} HooksOptions
  * @typedef {import('./lib/index.js').Options} Options
  * @typedef {import('./lib/index.js').UrlTransform} UrlTransform
  */
